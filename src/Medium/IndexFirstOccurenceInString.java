38e7865ecea32753e9b37324601cf4ff9bc30608
Ricky <ahujashaurya13@gmail.com>
2023-01-27 16:08:47 -0500

    optimized solution added for firstIndexOccurence

--- src/Medium/IndexFirstOccurenceInString.java
+++ src/Medium/IndexFirstOccurenceInString.java
@@ -22,6 +22,17 @@ public class IndexFirstOccurenceInString {
         return -1;
     }
 
+//    public int strStr(String haystack, String needle) {
+//        for(int i=0;i<haystack.length()-needle.length()+1;i++){
+//            if(haystack.charAt(i)==needle.charAt(0)){
+//                if(haystack.substring(i,needle.length()+i).equals(needle)){
+//                    return i;
+//                }
+//            }
+//        }
+//        return -1;
+//    }
+
     public static void main(String[] args) {
         String s1 = "banana";
         String s2 = "ana";
