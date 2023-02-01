50fab1bd74269aa6848d82c6714d4bd2118b75a3
Ricky <ahujashaurya13@gmail.com>
2023-02-01 01:32:30 -0500

    final solution to ragged compressor

--- src/PPS/RaggedCompressor.java
+++ src/PPS/RaggedCompressor.java
@@ -1,9 +1,9 @@
 package PPS;
 
+
+
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class RaggedCompressor {
     public static void main(String[] args) {
@@ -15,13 +15,15 @@ public class RaggedCompressor {
         for(int i = 0; i < numRows; i++){
             System.out.println("Enter num of elements in row " + (i+1) + " - ");
             int numElements = sc.nextInt();
-            raggedArr[i] = new int[numElements];
+            raggedArr = new int[numRows][numElements];
             System.out.println("Enter elements of row " + (i+1) + " - ");
 
             for(int j = 0; j < numElements; j++){
                 raggedArr[i][j] = sc.nextInt();
             }
         }
+
+        int[] duplicatesRemoved = new int[numRows];
 
         for(int i = 0; i < raggedArr.length; i++){
             boolean[] temp = new boolean[raggedArr[i].length];
@@ -35,6 +37,7 @@ public class RaggedCompressor {
                 }
             }
 
+            duplicatesRemoved[i] = raggedArr[i].length - count;
 
             int[] t = new int[count];
             int indexForT = 0;
@@ -44,23 +47,18 @@ public class RaggedCompressor {
                     indexForT++;
                 }
             }
-//            raggedArr[i] = new int[t.length];
             raggedArr[i] = t;
-//            System.out.println(Arrays.toString(raggedArr));
         }
-
 
         for(int i = 0; i < raggedArr.length; i++){
             for(int j = 0; j < raggedArr[i].length; j++){
                 System.out.print(raggedArr[i][j] + " ");
             }
-            System.out.println();
-//            System.out.println(raggedArr[i].length);
-            System.out.println();
         }
 
-//        int[] arr = {10, 1, 2};
-//        System.out.println(arr);
-
+//        for(int i = 0; i < duplicatesRemoved.length; i++){
+//            System.out.println();
+//        }
+        System.out.println(Arrays.toString(duplicatesRemoved));
     }
 }
