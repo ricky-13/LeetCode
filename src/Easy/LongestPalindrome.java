a916c719b83f55d6ac59517769d09f0d5c7e9615
Ricky <ahujashaurya13@gmail.com>
2023-02-02 15:37:54 -0500

    added code for longest palindrome

---
+++ src/Easy/LongestPalindrome.java
@@ -0,0 +1,29 @@
+package Easy;
+
+import java.util.HashMap;
+
+public class LongestPalindrome {
+    public static void longestPalindrome(String s) {
+        HashMap<Character, Integer> hm = new HashMap();
+
+        for(int i = 0; i < s.length(); i++) {
+            if (hm.containsKey(s.charAt(i))) {
+                int a = hm.get(s.charAt(i));
+                a++;
+                hm.replace(s.charAt(i), a);
+            } else {
+                hm.put(s.charAt(i), 1);
+            }
+        }
+
+//        for(int i = 0; i < )
+
+        System.out.println(hm);
+
+    }
+
+    public static void main(String[] args) {
+        String s = "abccccdd";
+        longestPalindrome(s);
+    }
+}
