36e824ff3bc97a795b8449293a3414c562304d29
Ricky <ahujashaurya13@gmail.com>
2023-02-01 20:57:37 -0500

    adding code for zigzag conversion

---
+++ src/Medium/ZigzagConversion.java
@@ -0,0 +1,12 @@
+package Medium;
+
+public class ZigzagConversion {
+    public String convert(String s, int numRows){
+        int[] stringBuilders = new int[numRows];
+        for(int i = 0; i < numRows; i++){
+            String str = "s" + i;
+//            stringBuilders[i] = new StringBuilder(str);
+        }
+        return s;
+    }
+}
