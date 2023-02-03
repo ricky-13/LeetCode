2993fe0d6c251095feccabb6e551fd827912647b
Ricky <ahujashaurya13@gmail.com>
2023-02-03 18:25:04 -0500

    added revised code for longestPalindrome

--- src/Easy/LongestPalindrome.java
+++ src/Easy/LongestPalindrome.java
@@ -1,11 +1,12 @@
 package Easy;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class LongestPalindrome {
-    public static void longestPalindrome(String s) {
-        HashMap<Character, Integer> hm = new HashMap();
-
+    public static int longestPalindrome(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        int count =0;
         for(int i = 0; i < s.length(); i++) {
             if (hm.containsKey(s.charAt(i))) {
                 int a = hm.get(s.charAt(i));
@@ -17,15 +18,24 @@ public class LongestPalindrome {
         }
 
 //        for(int i = 0; i < )
-        for(String str : s){
-
+        for(Map.Entry me : hm.entrySet()){
+            if((int)me.getValue() >= 2 && (int)me.getValue()%2 == 0){
+                count = count + (int)me.getValue();
+            }
+            else if((int)me.getValue() >= 2 && (int)me.getValue()%2 == 1){
+                count = count + (int)me.getValue() - 1;
+            }
         }
-        System.out.println(hm);
 
+        if(hm.containsValue(1)){
+            count = count + 1;
+        }
+
+        return count;
     }
 
     public static void main(String[] args) {
-        String s = "abccccdd";
-        longestPalindrome(s);
+        String s = "ccc";
+        System.out.println(longestPalindrome(s));
     }
 }
