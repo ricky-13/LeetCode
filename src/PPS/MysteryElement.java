131570fd32ed51116a4810aec2e66a4f55a09303
Ricky <ahujashaurya13@gmail.com>
2023-02-08 16:20:50 -0500

    changes to myseryElement

--- src/PPS/MysteryElement.java
+++ src/PPS/MysteryElement.java
@@ -1,31 +1,58 @@
 package PPS;
 
 public class MysteryElement {
-    public static void missingElement(int[] arr){
+//    public static void missingElement(int[] arr){
+//        int max = 0;
+//
+//        for(int i = 0; i< arr.length; i++){
+//            if(max < arr[i]){
+//                max = arr[i];
+//            }
+//        }
+//
+//        boolean[] parallel = new boolean[max+1];
+//// max + 1
+//        for (int i = 0; i < arr.length; i++)  {
+//            parallel[arr[i]] = true;
+//        }
+//
+//        for(int i = 1; i < parallel.length; i++){
+//            if(parallel[i] == false){
+//                System.out.println(i);
+//            }
+//        }
+//    }
+
+    public static int missingNumber(int[] nums) {
+
         int max = 0;
 
-        for(int i = 0; i< arr.length; i++){
-            if(max < arr[i]){
-                max = arr[i];
+        for(int i = 0; i< nums.length; i++){
+            if(max < nums[i]){
+                max = nums[i];
             }
+        }
+        if(max < nums.length){
+            max = nums.length;
         }
 
         boolean[] parallel = new boolean[max+1];
-// max + 1
-        for (int i = 0; i < arr.length; i++) {
-            parallel[arr[i]] = true;
-        }
 
-        for(int i = 1; i < parallel.length; i++){
+        for (int i = 0; i < nums.length; i++)  {
+            parallel[nums[i]] = true;
+        }
+//
+        for(int i = 0; i < parallel.length; i++){
             if(parallel[i] == false){
-                System.out.println(i);
+                return i;
             }
         }
+        return -1;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 7, 2, 3, 4, 5, 6, 8, 9, 10, 11};
-        missingElement(arr);
+        int[] arr = {0, 1};
+        System.out.println(missingNumber(arr));
     }
 }
 
