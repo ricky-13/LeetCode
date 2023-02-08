04d04452e4691b6bcaf3130c71ed4ca6ebf45db7
Ricky <ahujashaurya13@gmail.com>
2023-02-07 19:54:46 -0500

    code for reverse string

--- src/Easy/ReverseString.java
+++ src/Easy/ReverseString.java
@@ -8,14 +8,24 @@ public class ReverseString {
             s[s.length - 1 - i] = s[i];
             s[i] = tmp;
         }
-
-        for(char a : s){
-            System.out.print(a + " ");
-        }
+//        System.out.prin]");
     }
 
     public static void main(String[] args) {
         char[] s = {'h', 'e', 'l', 'l', 'o'};
+
         reverseString(s);
+
+//        ("[");
+//        for(int i = 0; i < s.length; i++){
+//            if(i == s.length - 1){
+//                System.out.print("\"" + s[i] + "\"");
+//            }
+//            else{
+//                System.out.print("\"" + s[i] + "\"" + ",");
+//            }
+//
+//        }
+//        System.out.print("
     }
 }
