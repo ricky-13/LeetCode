42e863f19d951ad3913bb2227956fdcd6ef71705
Ricky <ahujashaurya13@gmail.com>
2023-03-06 13:09:04 -0500

    last commit for this code

--- src/Medium/MinimumSizeSubarraySum.java
+++ src/Medium/MinimumSizeSubarraySum.java
@@ -3,38 +3,59 @@ package Medium;
 import java.util.Arrays;
 
 public class MinimumSizeSubarraySum {
-    public static void minSubArrayLen(int target, int[] nums) {
+    public static int minSubArrayLen(int target, int[] nums) {
 
         int min = Integer.MAX_VALUE;
         int currentSum = 0;
         int j = 0;
         int currentLength = 0;
         int i;
-        
+
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+
         for ( i = 0; i < nums.length; i++) {
             currentSum = currentSum + nums[i];
             currentLength++;
-            System.out.println(currentLength + "   " + currentSum);
-            while(currentSum > target){
+            while(currentSum >= target){
+                min = currentLength < min ? currentLength : min;
+
                 j++;
                 currentSum = currentSum - nums[j];
                 currentLength -= 1;
-                if(currentSum > target){
-                    min = currentLength < min ? currentLength : min;
-                }
+
             }
         }
-        System.out.println(min + "    " + i + "    " + j + "   ");
+
+        return min == Integer.MAX_VALUE ? 0 : min;
     }
+
+//    public int minSubArrayLen(int target, int[] nums) {
+//        if (nums == null || nums.length == 0)
+//            return 0;
+//
+//        int i = 0, j = 0, sum = 0, min = Integer.MAX_VALUE;
+//
+//        while (j < nums.length) {
+//            sum += nums[j++];
+//
+//            while (sum >= target) {
+//                min = Math.min(min, j - i);
+//                sum -= nums[i++];
+//            }
+//        }
+//
+//        return min == Integer.MAX_VALUE ? 0 : min;
+//    }
 
 
     public static void main(String[] args) {
-        int[] arr = {2,3, 1, 2, 4, 3};
+        int[] arr = {1,2,3,4,5};
 //        for (int i :
 //                arr) {
 //            System.out.println(i);
 //        }
-        minSubArrayLen(7, arr);
-
+        System.out.println(minSubArrayLen(15, arr));
     }
 }
