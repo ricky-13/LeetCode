e6b120539a4a1801e0d69749e090576d3720422f
Ricky <ahujashaurya13@gmail.com>
2024-04-19 15:47:38 -0400

    final merge sorted array

--- src/Easy/MergeSortedArray_88.java
+++ src/Easy/MergeSortedArray_88.java
@@ -3,11 +3,23 @@ package Easy;
 public class MergeSortedArray_88 {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int[] arr = new int[m+n];
-        for(int i = 0; i < m + n; i++){
-            int a = 0;
-            int b = 0;
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i < m+n; i++){
 
-            if(nums1[a] < nums2[b]){
+
+            if( a == m && b != n ){
+                arr[i] = nums2[b];
+                b++;
+                continue;
+            }
+            if( b == n && a != m ){
+                arr[i] = nums1[a];
+                a++;
+                continue;
+            }
+
+            if(nums1[a] < nums2[b] ){
                 arr[i] = nums1[a];
                 a++;
             } else {
@@ -16,8 +28,35 @@ public class MergeSortedArray_88 {
             }
         }
 
-        for(int i = 0; i < m + n; i++){
+        for (int i = 0; i < m+n; i++) {
             nums1[i] = arr[i];
+        }
+
+//        public void merge(int[] nums1, int m, int[] nums2, int n) {
+//            int i = m - 1;
+//            int j = n - 1;
+//            int k = m + n - 1;
+//
+//            while (j >= 0) {
+//                if (i >= 0 && nums1[i] > nums2[j]) {
+//                    nums1[k--] = nums1[i--];
+//                } else {
+//                    nums1[k--] = nums2[j--];
+//                }
+//            }
+//        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+
+        MergeSortedArray_88 obj = new MergeSortedArray_88();
+        obj.merge(nums1, 3, nums2, 3);
+
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println(nums1[i]);
         }
     }
 }
